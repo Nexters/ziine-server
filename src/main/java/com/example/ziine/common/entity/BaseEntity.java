16e619747aad1894877c1f295886a585857ca9b9
Y-Joo <wldudwn99@gmail.com>
2025-02-01 01:02:39 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/com/example/ziine/common/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.example.ziine.common.entity;
+
+import jakarta.persistence.*;
+import java.time.ZonedDateTime;
+
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Column(updatable = false)
+    private ZonedDateTime createdAt;
+
+    private ZonedDateTime modifiedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = ZonedDateTime.now();
+        this.modifiedAt = ZonedDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.modifiedAt = ZonedDateTime.now();
+    }
+}
+
