6280ccd1d3be2f6af6230385c1aa58dfabe42843
Y-Joo <wldudwn99@gmail.com>
2025-02-02 14:09:50 +0900

    feat: ResponseEntityExceptionHandler 상속하는 방식으로 변경

--- src/main/java/com/example/ziine/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ziine/common/exception/GlobalExceptionHandler.java
@@ -1,22 +1,38 @@
 package com.example.ziine.common.exception;
 
 import com.example.ziine.common.dto.ErrorResponseDto;
+import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 @Slf4j
 @RequiredArgsConstructor
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> createResponseEntity(@Nullable Object body, HttpHeaders headers,
+                                                          HttpStatusCode statusCode, WebRequest request) {
+        if (body instanceof ProblemDetail problemDetail) {
+            log.warn("ProblemDetail: {}", problemDetail);
+            return ResponseEntity.status(statusCode)
+                    .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR.getCode(), problemDetail.getDetail()));
+        }
+        log.error("Response body is not an instance of ProblemDetail. Body: {}", body);
+        return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
+                .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR.getCode(), ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
+    }
 
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ErrorResponseDto> handleBusinessException(
@@ -27,52 +43,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity
             .status(e.getErrorCode().getHttpStatus().value())
             .body(new ErrorResponseDto(e.getErrorCode()));
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(
-        final MethodArgumentNotValidException e,
-        final HttpServletRequest request
-    ) {
-        log.error("MethodArgumentNotValidException: {} Request Url: {}", e.getMessage(), request.getRequestURL());
-        ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.METHOD_ARGUMENT_NOT_VALID.getCode(),
-                                                                 e.getAllErrors().get(0).getDefaultMessage());
-
-        return ResponseEntity
-            .status(ErrorCode.METHOD_ARGUMENT_NOT_VALID.getHttpStatus().value())
-            .body(errorResponseDto);
-    }
-
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    protected ResponseEntity<ErrorResponseDto> handleMethodNotSupportException(
-        final HttpRequestMethodNotSupportedException e,
-        final HttpServletRequest request
-    ) {
-        log.error("HttpRequestMethodNotSupportedException: {} Request Url: {}", e.getMessage(), request.getRequestURL());
-        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
-            ErrorCode.METHOD_NOT_ALLOWED.getCode(),
-            e.getMessage()
-        );
-
-        return ResponseEntity
-            .status(ErrorCode.METHOD_NOT_ALLOWED.getHttpStatus().value())
-            .body(errorResponseDto);
-    }
-
-    @ExceptionHandler(NoResourceFoundException.class)
-    protected ResponseEntity<ErrorResponseDto> handleNoResourceFoundException(
-        final NoResourceFoundException e,
-        final HttpServletRequest request
-    ) {
-        log.error("NoResourceFoundException: {} Request Url: {}", e.getMessage(), request.getRequestURL());
-        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
-            ErrorCode.NO_RESOURCE_FOUND_EXCEPTION.getCode(),
-            e.getMessage()
-        );
-
-        return ResponseEntity
-            .status(ErrorCode.NO_RESOURCE_FOUND_EXCEPTION.getHttpStatus().value())
-            .body(errorResponseDto);
     }
 
     @ExceptionHandler(value = {Exception.class, RuntimeException.class, SQLException.class,
