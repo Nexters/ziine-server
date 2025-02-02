a05c430f86c5eab678b6fd40c94f2e614bac5c8f
jeyong <joon6093@naver.com>
2025-02-03 02:36:13 +0900

    chore: 어노테이션 숟서 변경

--- src/main/java/com/ziine/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ziine/common/exception/GlobalExceptionHandler.java
@@ -3,10 +3,8 @@ package com.ziine.common.exception;
 import com.ziine.common.dto.ErrorResponseDto;
 import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
-import java.sql.SQLException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
@@ -16,14 +14,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestControllerAdvice
 @Slf4j
 @RequiredArgsConstructor
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     public ResponseEntity<Object> createResponseEntity(@Nullable Object body, HttpHeaders headers,
-                                                          HttpStatusCode statusCode, WebRequest request) {
+                                                       HttpStatusCode statusCode, WebRequest request) {
         if (body instanceof ProblemDetail problemDetail) {
             log.warn("ProblemDetail: {}", problemDetail);
             return ResponseEntity.status(statusCode)
@@ -31,29 +29,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         }
         log.error("Response body is not an instance of ProblemDetail. Body: {}", body);
         return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
-                .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR.getCode(), ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
+                .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponseDto> handleBusinessException(
-        final BusinessException e,
-        final HttpServletRequest request
-    ) {
+    public ResponseEntity<ErrorResponseDto> handleBusinessException(final BusinessException e,
+                                                                    final HttpServletRequest request) {
         log.error("BusinessException: {} Request Url: {}", e.getErrorCode(), request.getRequestURL());
         return ResponseEntity
-            .status(e.getErrorCode().getHttpStatus().value())
-            .body(new ErrorResponseDto(e.getErrorCode()));
+                .status(e.getErrorCode().getHttpStatus().value())
+                .body(new ErrorResponseDto(e.getErrorCode()));
     }
 
-    @ExceptionHandler(value = {Exception.class})
-    public ResponseEntity<ErrorResponseDto> handleInternalException(
-        final Exception e,
-        final HttpServletRequest request
-    ) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleInternalException(final Exception e,
+                                                                    final HttpServletRequest request) {
         log.error("Request URL: {}", request.getRequestURL(), e);
-
         return ResponseEntity
-            .status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus().value())
-            .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR));
+                .status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus().value())
+                .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR));
     }
 }
