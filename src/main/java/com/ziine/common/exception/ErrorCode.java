455ecedc0c256084a475d3d809ac004988b2d8fb
jeyong <joon6093@naver.com>
2025-02-03 14:38:43 +0900

    refactor: 일반적인 예외처리를 간소화하도록 변경

--- src/main/java/com/ziine/common/exception/ErrorCode.java
+++ src/main/java/com/ziine/common/exception/ErrorCode.java
@@ -9,19 +9,36 @@ public enum ErrorCode {
     /**
      * 1XXX -> Common 에러
      */
-    BAD_REQUEST(1000, "Bad Request", HttpStatus.BAD_REQUEST),
-    NOT_FOUND(1001, "Contents Not Found", HttpStatus.NOT_FOUND),
-    METHOD_NOT_ALLOWED(1002, "Method Not Allowed", HttpStatus.METHOD_NOT_ALLOWED),
-    INTERNAL_SERVER_ERROR(1003, "Internal Server Error Occurred", HttpStatus.INTERNAL_SERVER_ERROR),
-    METHOD_ARGUMENT_NOT_VALID(1004, "Method Argument Is Not Valid", HttpStatus.BAD_REQUEST),
-    NO_RESOURCE_FOUND_EXCEPTION(1005, "No Resource Found", HttpStatus.NOT_FOUND)
+    BAD_REQUEST(
+            1000,
+            "Bad Request",
+            HttpStatus.BAD_REQUEST
+    ),
+    INTERNAL_SERVER_ERROR(
+            1001,
+            "Internal Server Error",
+            HttpStatus.INTERNAL_SERVER_ERROR
+    ),
+
+    /**
+     * 2XXX -> Artwork 에러
+     */
+    ARTWORK_NOT_FOUND(
+            2001,
+            "Artwork Not Found",
+            HttpStatus.BAD_REQUEST
+    ),
     ;
 
     private final int code;
     private final String message;
     private final HttpStatus httpStatus;
 
-    ErrorCode(final int code, final String message, final HttpStatus httpStatus) {
+    ErrorCode(
+            final int code,
+            final String message,
+            final HttpStatus httpStatus
+    ) {
         this.code = code;
         this.message = message;
         this.httpStatus = httpStatus;
--- src/main/java/com/ziine/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ziine/common/exception/GlobalExceptionHandler.java
@@ -3,12 +3,15 @@ package com.ziine.common.exception;
 import com.ziine.common.dto.ErrorResponseDto;
 import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -20,6 +23,33 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException e,
+            final HttpHeaders headers,
+            final HttpStatusCode statusCode,
+            final WebRequest request
+    ) {
+        final String message = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        final ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                statusCode,
+                message
+        );
+
+        return handleExceptionInternal(
+                e,
+                problemDetail,
+                headers,
+                statusCode,
+                request
+        );
+    }
+
+    @Override
     public ResponseEntity<Object> createResponseEntity(
             @Nullable Object body,
             HttpHeaders headers,
@@ -27,14 +57,30 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             WebRequest request
     ) {
         if (body instanceof ProblemDetail problemDetail) {
-            log.warn("ProblemDetail: {}", problemDetail);
+            log.info(
+                    "[CommonException] URI: {}, Status: {}, Message: {}",
+                    request.getDescription(false),
+                    statusCode,
+                    problemDetail.getDetail()
+            );
+
             return ResponseEntity.status(statusCode)
-                    .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR.getCode(), problemDetail.getDetail()));
+                    .body(new ErrorResponseDto(
+                            ErrorCode.BAD_REQUEST.getCode(),
+                            problemDetail.getDetail()
+                    ));
         }
-        log.error("Response body is not an instance of ProblemDetail. Body: {}", body);
+        log.error(
+                "[UnexpectedException] URI: {}, Message: {}",
+                request.getDescription(false),
+                body
+        );
+
         return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
-                .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR.getCode(),
-                        ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
+                .body(new ErrorResponseDto(
+                        ErrorCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ErrorCode.INTERNAL_SERVER_ERROR.getMessage()
+                ));
     }
 
     @ExceptionHandler(BusinessException.class)
@@ -42,9 +88,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             final BusinessException e,
             final HttpServletRequest request
     ) {
-        log.error("BusinessException: {} Request Url: {}", e.getErrorCode(), request.getRequestURL());
+        log.info(
+                "[BusinessException] URI: {}, Code: {}, Message: {}",
+                request.getRequestURI(),
+                e.getErrorCode()
+                        .getCode(),
+                e.getMessage()
+        );
+
         return ResponseEntity
-                .status(e.getErrorCode().getHttpStatus().value())
+                .status(e.getErrorCode()
+                                .getHttpStatus())
                 .body(new ErrorResponseDto(e.getErrorCode()));
     }
 
@@ -53,9 +107,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             final Exception e,
             final HttpServletRequest request
     ) {
-        log.error("Request URL: {}", request.getRequestURL(), e);
+        log.error(
+                "[UnexpectedException] URI: {}, Message: {}",
+                request.getRequestURI(),
+                e.getMessage()
+        );
+
         return ResponseEntity
-                .status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus().value())
+                .status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
                 .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR));
     }
 }
