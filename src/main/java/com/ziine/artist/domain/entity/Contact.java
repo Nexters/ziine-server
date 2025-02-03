30de85cee40023317c272dcd3433920be1255f3f
jeyong <joon6093@naver.com>
2025-02-03 16:10:20 +0900

    feat: ContactEntity 추가

---
+++ src/main/java/com/ziine/artist/domain/entity/Contact.java
@@ -0,0 +1,45 @@
+package com.ziine.artist.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "contact")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ContactType type;
+
+    @Column(nullable = false, length = 255)
+    private String value;
+
+    @Column(nullable = false)
+    private Long artistId;
+
+    public Contact(
+            final ContactType type,
+            final String value,
+            final Long artistId
+    ) {
+        this.type = type;
+        this.value = value;
+        this.artistId = artistId;
+    }
+}
