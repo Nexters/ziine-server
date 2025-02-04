fb0e69e00a109583b58f8a61065c45c38ad066e5
jeyong <joon6093@naver.com>
2025-02-04 18:45:22 +0900

    feat: ArtistRepository 추가

---
+++ src/main/java/com/ziine/artist/domain/repository/ArtistRepository.java
@@ -0,0 +1,10 @@
+package com.ziine.artist.domain.repository;
+
+import com.ziine.artist.domain.entity.ArtistEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistRepository extends JpaRepository<ArtistEntity, Long> {
+
+}
