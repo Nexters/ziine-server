49e3ca918383f0163abad967f0014f865a622219
Y-Joo <wldudwn99@gmail.com>
2025-02-07 00:43:37 +0900

    refactor: dto validation 조건 추가

--- src/main/java/com/ziine/domains/artwork/dto/request/ArtworkPersistRequestDto.java
+++ src/main/java/com/ziine/domains/artwork/dto/request/ArtworkPersistRequestDto.java
@@ -3,19 +3,23 @@ package com.ziine.domains.artwork.dto.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ziine.domains.artist.domain.entity.ContactType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.time.LocalDate;
 import java.util.List;
 
 public record ArtworkPersistRequestDto(
-    @NotBlank
+    @NotBlank(message = "Title is required.")
     String title,
+    @PositiveOrZero
     int width,
+    @PositiveOrZero
     int height,
-    @NotBlank
+    @NotBlank(message = "Metarial is required.")
     String material,
-    @NotBlank
+    @NotBlank(message = "Artwork image url is required.")
     String artworkImageUrl,
-    @NotBlank
+    @NotBlank(message = "Artist name is required.")
     String artistName,
     String description,
     List<String> educations,
@@ -23,16 +27,23 @@ public record ArtworkPersistRequestDto(
     List<ContactRequestDto> contacts,
     String email
 ) {
+
     public record ExhibitionRequestDto(
-        @NotBlank
+        @NotBlank(message = "Exhibition title is required.")
         String title,
         @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate exhibitionDate
-    ) {}
+    ) {
+
+    }
 
     public record ContactRequestDto(
+        @NotNull
         ContactType type,
+        @NotBlank
         String value
-    ) {}
+    ) {
+
+    }
 }
 
