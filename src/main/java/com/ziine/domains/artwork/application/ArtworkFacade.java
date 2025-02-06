57b38f4a66b9d83e3ea53a322dcd76bcc2b9b1f1
Y-Joo <wldudwn99@gmail.com>
2025-02-07 00:25:32 +0900

    chore: annotation 순서 변경

--- src/main/java/com/ziine/domains/artwork/application/ArtworkFacade.java
+++ src/main/java/com/ziine/domains/artwork/application/ArtworkFacade.java
@@ -10,8 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
 @RequiredArgsConstructor
+@Service
 public class ArtworkFacade {
     private final ArtworkService artworkService;
     private final ArtistService artistService;
