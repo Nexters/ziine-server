38e057ebfd5cd7d23a7c0b74cfa427cf4e1534c6
Y-Joo <wldudwn99@gmail.com>
2025-02-07 00:27:08 +0900

    chore: lazy loading 명시

--- src/main/java/com/ziine/domains/artist/domain/entity/ArtistEntity.java
+++ src/main/java/com/ziine/domains/artist/domain/entity/ArtistEntity.java
@@ -5,6 +5,7 @@ import com.ziine.domains.artwork.domain.entity.ArtworkEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -36,16 +37,16 @@ public class ArtistEntity {
     @Column(length = 255)
     private String email;
 
-    @OneToMany(mappedBy = "artistEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "artistEntity", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private final List<ArtworkEntity> artworkEntities = new ArrayList<>();
 
-    @OneToMany(mappedBy = "artistEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "artistEntity", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private final List<EducationEntity> educationEntities = new ArrayList<>();
 
-    @OneToMany(mappedBy = "artistEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "artistEntity", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private final List<ExhibitionEntity> exhibitionEntities = new ArrayList<>();
 
-    @OneToMany(mappedBy = "artistEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "artistEntity", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private final List<ContactEntity> contactEntities = new ArrayList<>();
 
     public ArtistEntity(final ArtistPersistDto artistPersistDto) {
