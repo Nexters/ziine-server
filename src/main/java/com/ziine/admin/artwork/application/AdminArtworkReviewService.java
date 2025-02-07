78603d800d59b6614e397f2320cf79a7043721a2
jeyong <joon6093@naver.com>
2025-02-07 16:20:56 +0900

    feat: Artwork의 상태 변화에 따라 메일을 발송하도록 구현

--- src/main/java/com/ziine/admin/artwork/application/AdminArtworkReviewService.java
+++ src/main/java/com/ziine/admin/artwork/application/AdminArtworkReviewService.java
@@ -4,6 +4,7 @@ import com.ziine.admin.artwork.application.exception.AdminNotFoundException;
 import com.ziine.admin.artwork.dto.request.AdminArtworkRejectRequestDto;
 import com.ziine.admin.auth.application.AdminContextHolder;
 import com.ziine.admin.auth.domain.Admin;
+import com.ziine.api.artwork.application.ArtworkStatusMailService;
 import com.ziine.api.artwork.application.exception.ArtworkNotFoundException;
 import com.ziine.api.artwork.domain.entity.ArtworkEntity;
 import com.ziine.api.artwork.domain.entity.ArtworkStatus;
@@ -20,6 +21,7 @@ public class AdminArtworkReviewService {
 
     private final ArtworkRepository artworkRepository;
     private final ArtworkStatusHistoryRepository artworkStatusHistoryRepository;
+    private final ArtworkStatusMailService artworkStatusMailService;
 
     @Transactional
     public void approveArtwork(final Long artworkId) {
@@ -31,9 +33,8 @@ public class AdminArtworkReviewService {
         final ArtworkStatus fromStatus = artworkEntity.getStatus();
         artworkEntity.updateStatus(ArtworkStatus.APPROVED);
 
-        artworkStatusHistoryRepository.save( // TODO. 추후 Spring Event 방식으로 변경하여 상태 변경을 저장하는 보조 관심사 분리
-            new ArtworkStatusHistoryEntity(fromStatus, ArtworkStatus.APPROVED,
-                null, admin.name(), artworkEntity));
+        persistArtworkStatusChange(fromStatus, ArtworkStatus.APPROVED,
+            null, admin.name(), artworkEntity);  // TODO. 추후 Spring Event 방식으로 변경하여 상태 변경을 저장하는 보조 관심사 분리
     }
 
     @Transactional
@@ -49,8 +50,21 @@ public class AdminArtworkReviewService {
         final ArtworkStatus fromStatus = artworkEntity.getStatus();
         artworkEntity.updateStatus(ArtworkStatus.REJECTED);
 
-        artworkStatusHistoryRepository.save( // TODO. 추후 Spring Event 방식으로 변경하여 상태 변경을 저장하는 보조 관심사 분리
-            new ArtworkStatusHistoryEntity(fromStatus, ArtworkStatus.REJECTED,
-                adminArtworkRejectRequestDto.rejectionReason(), admin.name(), artworkEntity));
+        persistArtworkStatusChange(fromStatus, ArtworkStatus.REJECTED,
+            adminArtworkRejectRequestDto.rejectionReason(), admin.name(), artworkEntity);
+        // TODO. 추후 Spring Event 방식으로 변경하여 상태 변경을 저장하는 보조 관심사 분리
+    }
+
+    private void persistArtworkStatusChange(
+        final ArtworkStatus fromStatus,
+        final ArtworkStatus toStatus,
+        final String reason,
+        final String modifiedBy,
+        final ArtworkEntity artworkEntity
+    ) {
+        final ArtworkStatusHistoryEntity artworkStatusHistoryEntity = artworkStatusHistoryRepository.save(
+            new ArtworkStatusHistoryEntity(fromStatus, toStatus, reason, modifiedBy, artworkEntity));
+
+        artworkStatusMailService.sendArtworkStatusChangeMail(artworkStatusHistoryEntity);
     }
 }
--- src/main/java/com/ziine/api/artwork/application/ArtworkService.java
+++ src/main/java/com/ziine/api/artwork/application/ArtworkService.java
@@ -11,13 +11,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @RequiredArgsConstructor
 @Service
 public class ArtworkService {
 
     private final ArtworkRepository artworkRepository;
     private final ArtworkStatusHistoryRepository artworkStatusHistoryRepository;
+    private final ArtworkStatusMailService artworkStatusMailService;
 
     @Transactional
     public ArtworkEntity persistArtwork(
@@ -27,11 +27,23 @@ public class ArtworkService {
         final ArtworkEntity artworkEntity = artworkRepository.save(
             ArtworkEntity.fromArtworkPersistDto(artworkPersistDto, artistEntity));
 
-        artworkStatusHistoryRepository.save(  // TODO. 추후 Spring Event 방식으로 변경하여 상태 변경을 저장하는 보조 관심사 분리
-            new ArtworkStatusHistoryEntity(ArtworkStatus.REQUEST, ArtworkStatus.PENDING,
-                null, artistEntity.getName(), artworkEntity)
-        );
+        persistArtworkStatusChange(ArtworkStatus.REQUEST, ArtworkStatus.PENDING,
+            null, artistEntity.getName(), artworkEntity);  // TODO. 추후 Spring Event 방식으로 변경하여 상태 변경을 저장하는 보조 관심사 분리
 
         return artworkEntity;
+    }
+
+    private void persistArtworkStatusChange(
+        final ArtworkStatus fromStatus,
+        final ArtworkStatus toStatus,
+        final String reason,
+        final String modifiedBy,
+        final ArtworkEntity artworkEntity
+    ) {
+        final ArtworkStatusHistoryEntity artworkStatusHistoryEntity = artworkStatusHistoryRepository.save(
+            new ArtworkStatusHistoryEntity(fromStatus, toStatus, reason, modifiedBy, artworkEntity)
+        );
+
+        artworkStatusMailService.sendArtworkStatusChangeMail(artworkStatusHistoryEntity);
     }
 }
