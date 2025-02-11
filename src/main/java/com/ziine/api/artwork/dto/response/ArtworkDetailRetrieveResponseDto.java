5c5e6be68653a9fb9df4d14e69d6a022fce1c6c5
Y-Joo <wldudwn99@gmail.com>
2025-02-11 23:19:48 +0900

    fix: 작품 상세 조회에 시간 정보 추가

--- src/main/java/com/ziine/api/artwork/dto/response/ArtworkDetailRetrieveResponseDto.java
+++ src/main/java/com/ziine/api/artwork/dto/response/ArtworkDetailRetrieveResponseDto.java
@@ -2,6 +2,7 @@ package com.ziine.api.artwork.dto.response;
 
 import com.ziine.api.artist.dto.response.ArtistDetailResponseDto;
 import com.ziine.api.artwork.domain.entity.ArtworkEntity;
+import java.time.ZonedDateTime;
 
 public record ArtworkDetailRetrieveResponseDto(
     Long id,
@@ -12,6 +13,8 @@ public record ArtworkDetailRetrieveResponseDto(
     String description,
     String artworkImageUrl,
     String shareUrl,
+    ZonedDateTime createdAt,
+    ZonedDateTime modifiedAt,
     ArtistDetailResponseDto artist
 ) {
 
@@ -27,7 +30,9 @@ public record ArtworkDetailRetrieveResponseDto(
             artworkEntity.getDescription(),
             artworkEntity.getImageUrl(),
             generateShareUrl(artworkEntity.getId()),
-            ArtistDetailResponseDto.fromEntity(artworkEntity.getArtistEntity())
+            artworkEntity.getCreatedAt(),
+            artworkEntity.getModifiedAt(),
+            com.ziine.api.artist.dto.response.ArtistDetailResponseDto.fromEntity(artworkEntity.getArtistEntity())
         );
     }
 
